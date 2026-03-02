a85d091873ba585865b88162f77842656a325b13
Samuel Abramov <info@abramov-samuel.de>
2026-03-02 13:18:03 +0100

    refactor: remove legacy BlockAlignment re-exports from AlignmentPlugin
    BlockAlignment is now exclusively exported from model/BlockAlignment.ts
    and the barrel index.ts. No legacy re-export paths through the plugin.

--- packages/core/src/plugins/alignment/index.ts
+++ packages/core/src/plugins/alignment/index.ts
@@ -2,7 +2,6 @@ export {
 	AlignmentPlugin,
 	ALIGNMENT_ICONS,
 	type AlignmentConfig,
-	type BlockAlignment,
 } from './AlignmentPlugin.js';
 
 export type { AlignmentLocale } from './AlignmentLocale.js';
